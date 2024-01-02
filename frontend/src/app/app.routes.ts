0a704610b2a8c5471aa6f56f1fe589f19790be02
Moustaquim Younes <moustaquimy@gmail.com>
2024-01-02 23:15:23 +0100

    Update app.routes.ts
    Adding Routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { CourspageComponent } from './courspage/courspage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"course_page", component: CourspageComponent}
+];
