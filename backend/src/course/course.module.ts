9f20f7946e1776702be1d83521e903e236c24dae
Soufiane Laaroussi <soufianelaaroussi03@gmail.com>
2024-01-04 18:12:48 +0100

    Added Course Module

---
+++ backend/src/course/course.module.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { CourseController } from './course.controller';
+import { CourseRepository } from './course.repository';
+import { CourseService } from './course.service';
+
+@Module({
+  controllers: [CourseController],
+	providers: [CourseService, CourseRepository],
+
+
+})
+export class Course {}
