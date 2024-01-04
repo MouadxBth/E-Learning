8226eaeb1840a539036fb523ad8850d2ca0e7934
Soufiane Laaroussi <soufianelaaroussi03@gmail.com>
2024-01-04 18:02:57 +0100

    Modified app.module.ts
    Importing Course And Chapter Modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,12 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Course } from './course/course.module';
+import { Chapter } from './chapter/chapter.module';
 
 @Module({
-  imports: [],
+  imports: [Course, Chapter],
   controllers: [AppController],
   providers: [AppService],
 })
